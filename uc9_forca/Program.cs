625a31ff8e351749904edd0640e7696dfc5035b1
LUCAS GOMES GALVAO <lucas.ggalvao@senacsp.edu.br>
2023-03-01 21:47:54 -0300

    lucas

---
+++ uc9_forca/Program.cs
@@ -0,0 +1,62 @@
+﻿int i, tentativas;
+int encontrado = 0;
+string letra;
+
+string[] palavrasecreta = new string[10];
+string[] palavravisivel = new string[10];
+
+Console.WriteLine("Informe o nome do " + (i + 1) );
+
+
+
+
+
+for (i = 0; i < 10; i++)
+{
+    Console.WriteLine((i + 1) + "ª Letra da palavra (total 10 letras");
+    palavrasecreta[i] = Console.ReadLine();
+}
+
+for (i = 0; i < 10; i++)
+{
+    Console.Write(palavrasecreta[i] + " ");
+    
+}
+Console.WriteLine("");
+Console.WriteLine("Pressione ENTER para continuar...");
+Console.ReadLine();
+
+Console.Clear();
+
+tentativas = 0;
+while (tentativas < 10)
+{
+    Console.Clear();
+    for (i = 0; i < 10; i++)
+    {
+        Console.Write(palavravisivel[i] + " ");
+    }
+    
+    Console.WriteLine("Qual letra deseja tentar?");
+    letra = Console.ReadLine();
+
+    for (i = 0;i < 10; i++)
+    {
+        if (letra == palavrasecreta[i])
+        {
+            palavravisivel[i] = letra;
+        }
+    }
+    
+    if (encontrado == 0)
+    {
+        Console.WriteLine("Letra nao encontrada");
+    }
+
+    encontrado = 0;
+    tentativas++;
+
+    Console.WriteLine("");
+    Console.WriteLine("Pressione ENTER para continuar...");
+    Console.ReadLine();
+}
